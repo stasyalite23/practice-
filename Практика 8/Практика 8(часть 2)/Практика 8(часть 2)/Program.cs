d0000d3d93e5d1162f3c30d0597981cbb5b37123
Анастасия <141082183+stasyalite23@users.noreply.github.com>
2024-01-10 15:49:47 +0300

    Add files via upload

---
+++ Практика 8/Практика 8(часть 2)/Практика 8(часть 2)/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Практика_8_часть_2_
+{
+    class Word
+    {
+        public string Origin { get; set; }
+        public string Translate { get; set; }
+        public Word(string origin, string translate)
+        {
+            Origin = origin;
+            Translate = translate;
+        }
+    }
+    class Dictionary
+    {
+        private IList<Word> _words = new List<Word>();
+        public string this [string origin]
+        {
+            get => FindWord(origin).Translate;
+            set => FindWord(origin).Translate = value;
+        }
+        public void AddWord(Word word) => _words.Add(word);
+        private Word FindWord (string origin)
+        {
+            Word word = null;
+            foreach (var item in _words)
+                if (item.Origin == origin)
+                {
+                    word = item;
+                    break;
+                }
+            return word;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary dictionary = new Dictionary();
+            dictionary.AddWord(new Word("Water", "Вода"));
+            dictionary.AddWord(new Word("Fire", "Огонь"));
+            Console.WriteLine(dictionary["Water"]);
+            Console.WriteLine(dictionary["Fire"]);
+        }
+    }
+}
---
+++ Практика 8/Практика 8/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Практика_8
+{
+    class FootballPlayer
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public FootballPlayer(string name, int number)
+        {
+            Name = name;
+            Number = number;
+        }
+        public override string ToString() => $"{Number}:{Name}";
+    }
+    class FootballTeam
+    {
+        private readonly FootballPlayer[] _players = new FootballPlayer[11];
+        public FootballTeam() { }
+        public FootballTeam(FootballPlayer[] players)
+        {
+            if (players.Length != 11)
+                throw new ArgumentOutOfRangeException("Игроков должно быть 11 человек");
+            _players = players;
+        }
+        public FootballPlayer this[int index]
+        {
+            get => CheckIfIndexCorrect(index) ? _players[index] : null;
+            set
+            {
+                if (!CheckIfIndexCorrect(index))
+                    return;
+                _players[index] = value;
+            }
+        }
+        private bool CheckIfIndexCorrect (int index) => index >= 0 && index <= 10;
+    }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                FootballTeam team = new FootballTeam();
+                team[8] = new FootballPlayer("Arasakov", 25);
+                Console.WriteLine(team[8]);
+                Console.WriteLine(team[8] is null);
+            }
+        }
+}
+
